674a4c26c7099ff87e5f09c72ce3637095081922
Dustin Yochim <yochim.dustin@gmail.com>
2023-02-22 19:30:01 -0600

    Add contact page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import loadHome from "./home";
 import loadMenu from "./menu";
+import loadContact from "./contact";
 import "./reset.css";
 import "./style.css";
 
-// loadHome();
-loadMenu();
+loadHome();
+
+const home = document.querySelector('[href="#home"]');
+home.addEventListener("click", loadHome());
+
+const menu = document.querySelector('[href="#menu"]');
+menu.addEventListener("click", loadMenu());
+
+const contact = document.querySelector('[href="#contact"]');
+contact.addEventListener("click", loadContact());
